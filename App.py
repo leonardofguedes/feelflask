4436fc2a0d3fc840e8872fe8d047ab6be5f13c22
Leonardo Forgiarini Guedes <43677896+leonfogue@users.noreply.github.com>
2022-03-31 20:42:29 -0300

    Update App.py

--- App.py
+++ App.py
@@ -4,10 +4,10 @@ from resources.feel import Feels, Feel
 
 
 app = Flask(__name__)
-api = Api(app) #ele vai fazer o gerencimento de como tudo funciona
+api = Api(app) 
 
 
-api.add_resource(Feels, '/feels') #colocamos o nome da classe e de onde queremos que seja chamado
+api.add_resource(Feels, '/feels') 
 api.add_resource(Feel, '/feels/<string:name>')
 if __name__ == '__main__':
     app.run(debug=True)
