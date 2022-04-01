3d9265e1662f86a08b951b47e447ef50e173f17e
Leonardo Forgiarini Guedes <43677896+leonfogue@users.noreply.github.com>
2022-04-01 15:57:48 -0300

    Update feel.py

--- feel.py
+++ feel.py
@@ -1,58 +1,102 @@
-from flask_restful import Resource, reqparse
-from models.feel import FeelModel
+from flask import abort
+from flask_restful import Resource, reqparse, marshal
+from models.feel1 import Feel
 
-
-subjects = [
-    {'name' : 'trabalho', 'feel': 50},
-    {'name' : 'flamengo', 'feel': 55}
+feels = [
+    Feel(name='trabalho', feel=50),
+    Feel(name='flamengo', feel=55),
 ]
 
 
-class Feels(Resource): #recurso da api,todo recurso tem funç. get, post, delete
+class FeelsAPI(Resource):
+    """
+    FeelsAPI permite acessar a coleção de objetos do tipo Feel.
+
+    métodos:
+    - GET: retorna uma lista com todos os objetos salvos
+    - POST: cria/adiciona um novo objeto na coleção
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            type=str,
+            required=True,
+            help='No Feel name provided',
+            location='json',
+        )
+        self.reqparse.add_argument(
+            'feel',
+            type=int,
+            default=0,
+            location='json',
+        )
+        super().__init__()
+
     def get(self):
-        return {'assunto': subjects} #o flaskrestful converterá isso para json, se tudo ocorrer bem
+        return {'feels': [marshal(f, Feel.fields) for f in
+                          feels]}  # o flaskrestful converterá isso para json, se tudo ocorrer bem
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        feelin = Feel(**args)
+        feels.append(feelin)
+        return marshal(feelin, Feel.fields), 201  # sucesso em post (created)
 
 
-class Feel(Resource):
-    atributos = reqparse.RequestParser()
-    atributos.add_argument('feel')
+class FeelAPI(Resource):
+    """
+    FeelAPI permite acessar um objeto especifico da coleção de Feels
 
+    métodos:
+    - GET: retorna um objeto pelo nome
+    - PUT: atualiza um objeto pelo nome
+    - DELETE: remove um objeto pelo nome
+    """
 
-    def find_feel(name):
-        for feel_n in subjects:
-            if feel_n['name'] == name:
-                return feel_n
-        return None
-
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            type=str,
+            required=True,
+            help='No Feel name provided',
+            location='json',
+        )
+        self.reqparse.add_argument(
+            'feel',
+            type=int,
+            default=0,
+            location='json',
+        )
+        super().__init__()
 
     def get(self, name):
-        feel = Feel.find_feel(name)
+        feel = self._find_feel(name)
         if feel:
-            return feel
-        return {'message': 'Feel not found'}, 404 #statuscode http not found
+            return marshal(feel, Feel.fields)
+        return {'message': 'Feel not found'}, 404  # statuscode http not found
 
+    def put(self, name):
+        feel = self._find_feel(name)
+        if not feel:
+            abort(404)
 
-    def post(self,name):
-        dados = Feel.atributos.parse_args() #os dados serão a chave e valor do argumento passado
-        feelin = FeelModel(name, **dados)
-        novo_feel = feelin.json()
-        subjects.append(novo_feel)
-        return novo_feel, 200 #codigo de sucesso
+        args = self.reqparse.parse_args()
+        feel.update(args['feel'])
+        return marshal(feel, Feel.fields)
 
+    def delete(self, name):
+        feel = self._find_feel(name)
+        if not feel:
+            abort(404)
 
-    def put(self,name):
-        dados = Feel.atributos.parse_args()  # os dados serão a chave e valor do argumento passado
-        feelin = FeelModel(name, **dados)
-        novo_feel = feelin.json()
-        feel = Feel.find_feel(name)
-        if feel:
-            feel.update(novo_feel) #quando existe, devolve dados
-            return novo_feel, 200 #ok
-        subjects.append(novo_feel)
-        return novo_feel, 201 #criado
+        feels.remove(feel)
+        return {'message': 'Subject deleted.'}
 
-
-    def delete(self,name):
-        global subjects
-        subjects = [ feel for feel in subjects if feel['name'] != name]
-        return { 'message': 'Subject deleted.'}
+    def _find_feel(self, name):
+        for feel in feels:
+            if feel.name == name:
+                return feel
+        return None
